4af57639937dfd953a6379245fb2a5e229d1537f
Danny <nguyenduongpa@gmail.com>
2025-02-02 09:06:54 -0800

    Add value

--- yeager/chunk.h
+++ yeager/chunk.h
@@ -1,20 +1,25 @@
 #ifndef yeager_chunk_h
 #define yeager_chunk_h
 
+#include "value.h"
 #include "common.h"
 
 typedef enum {
+    OP_CONSTANT,
     OP_RETURN,
 } OP_CODE;
 
 typedef struct {
     int count;
     int capacity;
+    int lines;
     uint8_t *code;
+    ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk *chunk);
-void writeChunk(Chunk *chunk, uint8_t byte);
+void writeChunk(Chunk *chunk, uint8_t byte, int line);
+int addConstant(Chunk *chunk, Value value);
 void freeChunk(Chunk *chunk);
 
 #endif
---
+++ yeager/value.h
@@ -0,0 +1,18 @@
+#ifndef yeager_value_h
+#define yeager_value_h
+
+#include "common.h"
+
+typedef double Value;
+
+typedef struct {
+    int count;
+    int capacity;
+    Value *values;
+} ValueArray;
+
+void initValueArray(ValueArray *array);
+void writeValueArray(ValueArray *array, Value value);
+void freeValueArray(ValueArray *array);
+
+#endif
