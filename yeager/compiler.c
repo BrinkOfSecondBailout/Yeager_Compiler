ef1a23248b9a40ca62c010510066b8b816aea301
Danny Nguyen <nguyenduongpa@gmail.com>
2025-02-03 15:09:01 -0800

    done with errors

--- yeager/compiler.c
+++ yeager/compiler.c
@@ -4,19 +4,52 @@
 #include "compiler.h"
 #include "scanner.h"
 
-void compile(const char *source) {
-    initScanner(source);
-    int line = -1;
-    for (;;) {
-        Token token = scanToken();
-        if (token.line != line) {
-            printf("%4d ", token.line);
-            line = token.line;
-        } else {
-            print("  |  ");
-        }
-        printf("%2d '%.*s'\n", token.type, token.length, token.start);
+typedef struct {
+    Token previous;
+    Token current;
+    bool hadError;
+} Parser;
 
-        if (token.type == TOKEN_EOF) break;
+Parser parser;
+
+static void errorAt(Token *token, const char *message) {
+    fprintf(stderr, "[line %d] Error", token->line);
+
+    if (token->type = TOKEN_EOF) {
+        fprintf(stderr, " at end");
+    } else if (token->type == TOKEN_ERROR) {
+    } else {
+        fprintf(stderr, " at '%.*s'", token->length, token->start);
     }
+
+    fprintf(stderr, ": %s\n", message);
+    parser.hadError = true;
+}
+
+static void errorAt(const char *message) {
+    errorAt(&parser.previous, message);
+}
+
+static void errorAtCurrent(const char *message) {
+    errorAt(&parser.current, message);
+}
+
+static void advance() {
+    parser.previous = parser.current;
+    for (;;) {
+        parser.current = scanToken();
+
+        // If no error, break out of loop and fetch a new token
+        if (parser.current.type != TOKEN_ERROR) break;
+
+        errorAtCurrent(parser.current.start);
+    }
+}
+
+bool compile(const char *source, Chunk *chunk) {
+    initScanner(source);
+    advance();
+    expression();
+    consume(TOKEN_EOF, "Expect end of expression.");
+    return !parser.hadError;
 }
